e6c6f69177bf77d763808f36c7df6bbecc8e9978
leo zman <leozman15@gmail.com>
2026-04-01 23:43:11 +0000

    Try fixing this error: `Console Error: An empty string ("") was passed t

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -23,13 +23,22 @@ Avatar.displayName = AvatarPrimitive.Root.displayName
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image
-    ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
-    {...props}
-  />
-))
+>(({ className, src, ...props }, ref) => {
+  // Fix for: "An empty string ("") was passed to the src attribute"
+  // If src is an empty string, we don't render the image component to avoid browser warnings and extra network requests.
+  if (src === "") {
+    return null
+  }
+  
+  return (
+    <AvatarPrimitive.Image
+      ref={ref}
+      src={src}
+      className={cn("aspect-square h-full w-full", className)}
+      {...props}
+    />
+  )
+})
 AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
 const AvatarFallback = React.forwardRef<
